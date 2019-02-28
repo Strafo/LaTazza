abe0b75b404f783961a0a2faf4a9c0a4abc4dd67
strafoxultrapowa <aspem2015@gmail.com>
2019-02-28 21:34:59 +0100

    minor change

--- guiIS/src/gui/LaTazzaFrame.java
+++ guiIS/src/gui/LaTazzaFrame.java
@@ -12,23 +12,24 @@ import guiConfig.LaTazzaFrameProperties;
 public class LaTazzaFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+    private LaTazzaFrameProperties laTazzaFrameProperties=new LaTazzaFrameProperties();
 
-	private LaTazzaFrameProperties laTazzaFrameProperties=new LaTazzaFrameProperties();
     private ContentPane contentPane=new ContentPane(this);
     private MenuPane menuPane=new MenuPane(this);
     private TopBarPane topBarPanePane =new TopBarPane(this);
 
-    public enum JPanelsNames{
+    public  enum JPanelsNames{
         STATOPANE,
         REGVENDITEPANE,
         REGPAGAMENTOPANE,
         REGRIFORNIMENTOPANE,
         GESTIONEPERSONALEPANE
+
     }
 
 
 
-	private Map<JPanelsNames,JPanel> jPanelMap =new HashMap<>();
+	private Map<JPanelsNames,JPanel> jPanelsMap =new HashMap<>();
 
 	/**
 	 * Create the frame.
@@ -38,34 +39,26 @@ public class LaTazzaFrame extends JFrame {
 		this.setBounds(laTazzaFrameProperties.getX(), laTazzaFrameProperties.getY(), laTazzaFrameProperties.getWidth(), laTazzaFrameProperties.getHeight());
 		this.setDefaultCloseOperation(laTazzaFrameProperties.getCloseOp());//todo checksetBounds(100, 100, 800, 500);
 		this.setUndecorated(true);
-
-
-
-		//todo check return value
-		jPanelMap.put(JPanelsNames.STATOPANE,new Stato());
-		jPanelMap.put(JPanelsNames.REGVENDITEPANE,new RegistraVendite());
-		jPanelMap.put(JPanelsNames.REGPAGAMENTOPANE,new RegistraPagamento());
-		jPanelMap.put(JPanelsNames.REGRIFORNIMENTOPANE,new RegistraRifornimento());
-		jPanelMap.put(JPanelsNames.GESTIONEPERSONALEPANE,new GestionePersonale());
-
-
         this.setContentPane(contentPane);
         this.add(contentPane);
         this.add(menuPane);
         this.add(topBarPanePane);
-        jPanelMap.forEach((k,v)->this.add(v));//aggiunge tutti i pannelli al frame
+
+
+		//todo check return value
+		jPanelsMap.put(JPanelsNames.STATOPANE,new StatoPane());
+		jPanelsMap.put(JPanelsNames.REGVENDITEPANE,new RegistraVendite());
+		jPanelsMap.put(JPanelsNames.REGPAGAMENTOPANE,new RegistraPagamento());
+		jPanelsMap.put(JPanelsNames.REGRIFORNIMENTOPANE,new RegistraRifornimento());
+		jPanelsMap.put(JPanelsNames.GESTIONEPERSONALEPANE,new GestionePersonale());
+
+
+
+        jPanelsMap.forEach((k,v)->this.add(v));//aggiunge tutti i pannelli al frame
 
 
 	}
-	
-	public void setLocationCenter() {
-		
-		Dimension objDimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int iCoordX = (objDimension.width - getWidth()) / 2;
-        int iCoordY = (objDimension.height - getHeight()) / 2;
-        setLocation(iCoordX, iCoordY); 
-		
-	}
+
 
     /**
      * Questo metodo peremette di settare la visibilità del pannello con nome panelName.
@@ -73,8 +66,21 @@ public class LaTazzaFrame extends JFrame {
      * @param panelVisibilityState lo stato (true/false) della visibilità del pannello
      */
 	public void setJPanelVisibleState(JPanelsNames panelName,boolean panelVisibilityState){
-	    jPanelMap.get(panelName).setVisible(panelVisibilityState);
+	    jPanelsMap.get(panelName).setVisible(panelVisibilityState);
     }
-	
+
+    public JPanel getJPanelByName(JPanelsNames name){
+	    return jPanelsMap.get(name);
+    }
+
+
+    public void setLocationCenter() {
+
+        Dimension objDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int iCoordX = (objDimension.width - getWidth()) / 2;
+        int iCoordY = (objDimension.height - getHeight()) / 2;
+        setLocation(iCoordX, iCoordY);
+
+    }
 
 }
