a4ea0c3035e41a0f2b6155df26362b219f9b1af3
simocampi <simocampy@gmail.com>
2019-03-09 16:00:47 +0100

    Creazioni Classi di Test

---
+++ guiIS/src/testBackend/ControllerDebitoTest.java
@@ -0,0 +1,12 @@
+package testBackend;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerDebitoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ guiIS/src/testBackend/EuroTest.java
@@ -0,0 +1,24 @@
+package testBackend;
+
+import backend.Euro;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EuroTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    void TestgetValore() {
+
+    }
+
+    @Test
+    void equals() {
+    }
+}
