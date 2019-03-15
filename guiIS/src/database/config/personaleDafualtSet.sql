0c03276b5e9e82c6dbcfe68d1e4471a17e838be4
strafoxultrapowa <aspem2015@gmail.com>
2019-03-15 16:57:12 +0100

    aggiunto script sql per la creazione del personale per il test

---
+++ guiIS/src/database/config/personaleDafualtSet.sql
@@ -0,0 +1,28 @@
+
+
+set search_path to LATAZZASCHEMA;
+
+insert into PERSONALE (nome,cognome,attivo) VALUES ('andrea','straforini',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('gabriele','armanino',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('jacopo','dapueto',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('simone','campisi',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('alessandro','caroti',false );
+insert into PERSONALE (nome,cognome,attivo) VALUES ('andrea','storace',false );
+insert into PERSONALE (nome,cognome,attivo) VALUES ('elisa','zazzera',false);
+
+insert into PERSONALE (nome,cognome,attivo) VALUES ('andrea','oneto',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('gabriele','gianluigino',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('jacopo','sivori',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('simone','mirto',true);
+insert into PERSONALE (nome,cognome,attivo) VALUES ('alessandro','sabatini',false );
+insert into PERSONALE (nome,cognome,attivo) VALUES ('andrea','squillante',false );
+insert into PERSONALE (nome,cognome,attivo) VALUES ('elisa','montezemolo',true);
+
+
+
+
+
+
+
+
+
