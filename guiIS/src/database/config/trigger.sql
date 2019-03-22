06ee7658c1185f62f22d33b9dccc05658f58ea9c
simocampi <simocampy@gmail.com>
2019-03-22 15:31:52 +0100

    Inizio Insert, Creazione Classe TestTrigger

--- guiIS/src/database/config/trigger.sql
+++ guiIS/src/database/config/trigger.sql
@@ -2,9 +2,9 @@
 set schema LATAZZASCHEMA;
 
 create  view  Magazzino(tipoCialda, qta) as
-  select tipoCialda, sum(qta*50)
+  select TIPO_CIALDA, sum(qta*50)
   from  LATAZZASCHEMA.RIFORNIMENTO
-  group by tipoCialda
+  group by TIPO_CIALDA
 
     except
 
@@ -19,29 +19,6 @@ create  view  Magazzino(tipoCialda, qta) as
   group by tipo_cialda;
 
 
-
-
-
-
-CREATE OR REPLACE FUNCTION checkNumCialde() RETURNS trigger AS
-$$
-declare
-num1 integer ;
-num2 integer;
-num_magaz integer;
-BEGIN
-
-num1:=(select sum(numero_cialde) from LATAZZASCHEMA.COMPRA_VISITATORE
-        where tipo_cialda = new.tipo_cialda);
-num2:=(select sum(numero_cialde) from LATAZZASCHEMA.COMPRA_DIPENDENTE
-        where tipo_cialda = new.tipo_cialda);
-num_magaz:=(select sum(qta*50) from LATAZZASCHEMA.MAGAZZINO
-	  	where tipoCialda=new.TipoCialda);
-if(num1+num2 > num_magaz) then  RAISE EXCEPTION 'Cialde di tipo % nel magazzino insufficienti', new.TipoCialda;
-end if;
-
-END;
-$$ LANGUAGE plpgsql;
 
 
 CREATE TRIGGER check_numero_aquisto_scatole
