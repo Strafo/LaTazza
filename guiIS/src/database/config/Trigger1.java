788a9770e311830d75053f36a2e92e38a1ba9bc6
simocampi <simocampy@gmail.com>
2019-03-22 12:56:33 +0100

    Trigger1 INIT

--- guiIS/src/database/config/Trigger1.java
+++ guiIS/src/database/config/Trigger1.java
@@ -1,66 +1,55 @@
 package database.config;
 import org.h2.api.Trigger;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Trigger1 implements Trigger {
 
-    // private Connection conn;
-    //private Statement stat;
-    //private String triggerName, table,schema;
-    //private boolean before;
-    //private int type; //1: Insert, 2: Update, 4: Delete, 8: select
-    //private ResultSet cV,cP,magazzino;
+    private Connection conn;
+    private Statement stat;
+    private String triggerName, table,schema;
+    private boolean before;
+    private int type; //1: Insert, 2: Update, 4: Delete, 8: select
+    private ResultSet cV,cP,magazzino;
 
     @Override
     public void init(Connection connection, String s, String t1, String t2, boolean b, int i) throws SQLException {
+        conn=connection; schema=s; triggerName =t1; table =t2; before=b; type=i;
 
-        //conn=connection; schema=s; triggerName =t1; table =t2; before=b; type=i;
-        //stat=conn.createStatement();
-        /*cV=stat.executeQuery("select sum(numero_cialde) "+
-                " from LATAZZASCHEMA.COMPRA_VISITATORE T "+
-                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_VISITATORE where T.TIPO_CIALDA=COMPRA_VISITATORE.TIPO_CIALDA); ");
+        cV=stat.executeQuery("select sum(numero_cialde)\n"+
+                                " from LATAZZASCHEMA.COMPRA_VISITATORE\n"+
+                                " where tipo_cialda = new.tipo_cialda;");
         cP=stat.executeQuery("select sum(numero_cialde)\n"+
                 " from LATAZZASCHEMA.COMPRA_DIPENDENTE\n"+
-                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_DIPENDENTE T where T.TIPO_CIALDA=COMPRA_DIPENDENTE.TIPO_CIALDA);");
-
+                " where tipo_cialda = new.tipo_cialda;");
         magazzino=stat.executeQuery("select sum(QTA*50)\n"+
                 " from LATAZZASCHEMA.MAGAZZINO\n"+
-                " where tipoCialda=(select TIPOCIALDA from LATAZZASCHEMA.MAGAZZINO T where T.TIPOCIALDA=MAGAZZINO.TIPOCIALDA);");
-        */
+                " where tipoCialda=new.TipoCialda;");
+
+
     }
 
-    /* public Trigger1(Connection conn) throws SQLException {
+    public Trigger1(String URL){
 
+    }
 
-         init(conn,"LATAZZASCHEMA","CHECKNUMCIALDE","LATAZZASCHEMA.COMPRA_VISITATORE",false,1);
-
-     }
- */
     @Override
-    public void fire(Connection conn, Object[] oldRow, Object[] newRow) throws SQLException {
-        PreparedStatement stat;
-        ResultSet cV, cP, magazzino;
-        String q1="select sum(numero_cialde) " +
-                " from LATAZZASCHEMA.COMPRA_VISITATORE T " +
-                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_VISITATORE where T.TIPO_CIALDA=COMPRA_VISITATORE.TIPO_CIALDA); ";
-        String q2="select sum(numero_cialde)\n" +
-                " from LATAZZASCHEMA.COMPRA_DIPENDENTE\n" +
-                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_DIPENDENTE T where T.TIPO_CIALDA=COMPRA_DIPENDENTE.TIPO_CIALDA);";
-        String q3="select sum(QTA*50)\n" +
-                " from LATAZZASCHEMA.MAGAZZINO\n" +
-                " where tipoCialda=(select TIPOCIALDA from LATAZZASCHEMA.MAGAZZINO T where T.TIPOCIALDA=MAGAZZINO.TIPOCIALDA);";
+    public void fire(Connection connection, Object[] oldRow, Object[] newRow) throws SQLException {
 
-        stat= conn.prepareStatement(q1); cV=stat.executeQuery(q1);
-        stat= conn.prepareStatement(q2); cP = stat.executeQuery(q2);
-        stat= conn.prepareStatement(q3); magazzino=stat.executeQuery(q3);
-        magazzino = stat.executeQuery();
-
-        cV.next();
-        cP.next();
-        magazzino.next();
-        if ((cV.getInt((Integer) newRow[0]) + cP.getInt((Integer) newRow[0])) > magazzino.getInt((Integer) newRow[0]))
-            throw new SQLException("Numero di cialde da comprare superiore a quelle disponibili in magazzino.");
+        if(newRow != null)
+            if()
+        cV=stat.executeQuery("select sum(numero_cialde)\n"+
+                " from LATAZZASCHEMA.COMPRA_VISITATORE\n"+
+                " where tipo_cialda = new.tipo_cialda;");
+        cP=stat.executeQuery("select sum(numero_cialde)\n"+
+                " from LATAZZASCHEMA.COMPRA_DIPENDENTE\n"+
+                " where tipo_cialda = new.tipo_cialda;");
+        magazzino=stat.executeQuery("select sum(QTA*50)\n"+
+                " from LATAZZASCHEMA.MAGAZZINO\n"+
+                " where tipoCialda=new.TipoCialda;");
 
     }
 
@@ -72,5 +61,25 @@ public class Trigger1 implements Trigger {
     @Override
     public void remove() throws SQLException {
 
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public String getTriggerName() {
+        return triggerName;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public int getType() {
+        return type;
     }
 }
