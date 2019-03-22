0dd5f96f39199e96be247a04f186fa15b9408fde
simocampi <simocampy@gmail.com>
2019-03-22 14:32:46 +0100

    Trigger1 INIT

--- guiIS/src/backend/database/config/Trigger1.java
+++ guiIS/src/backend/database/config/Trigger1.java
@@ -17,15 +17,14 @@ public class Trigger1 implements Trigger {
         conn=connection; schema=s; triggerName =t1; table =t2; before=b; type=i;
 
         cV=stat.executeQuery("select sum(numero_cialde)\n"+
-                                " from LATAZZASCHEMA.COMPRA_VISITATORE\n"+
-                                " where tipo_cialda = new.tipo_cialda;");
+                                " from LATAZZASCHEMA.COMPRA_VISITATORE\n T"+
+                                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_VISITATORE where T.TIPO_CIALDA=COMPRA_VISITATORE.TIPO_CIALDA); ");
         cP=stat.executeQuery("select sum(numero_cialde)\n"+
                 " from LATAZZASCHEMA.COMPRA_DIPENDENTE\n"+
-                " where tipo_cialda = new.tipo_cialda;");
+                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_DIPENDENTE T where T.TIPO_CIALDA=COMPRA_DIPENDENTE.TIPO_CIALDA);");
         magazzino=stat.executeQuery("select sum(QTA*50)\n"+
                 " from LATAZZASCHEMA.MAGAZZINO\n"+
-                " where tipoCialda=new.TipoCialda;");
-
+                " where tipoCialda=(select TIPOCIALDA from LATAZZASCHEMA.MAGAZZINO T where T.TIPOCIALDA=MAGAZZINO.TIPOCIALDA);");
 
     }
 
@@ -37,9 +36,6 @@ public class Trigger1 implements Trigger {
 
     @Override
     public void fire(Connection connection, Object[] oldRow, Object[] newRow) throws SQLException {
-
-        cV.next(); cP.next(); magazzino.next();
-
 
     }
 
