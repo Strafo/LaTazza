38672680e585ca04c79359ff1fa2ef3a93fe0629
simocampi <simocampy@gmail.com>
2019-03-22 13:34:42 +0100

    Trigger1 INIT

---
+++ guiIS/src/database/config/TriggersTest.java
@@ -0,0 +1,13 @@
+package database.config;
+
+import java.sql.SQLException;
+
+public class TriggersTest {
+
+    private static final String URL="jdbc:h2:file:C:/Users/simoc/IdeaProjects/LaTazza/guiIS/src/database/config";
+    public static void main(String[] args) throws SQLException {
+
+        Trigger1 trigger1= new Trigger1(URL);
+
+    }
+}
