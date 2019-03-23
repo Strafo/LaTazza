ed1db56b467208480e88f90f6f5041b2c85a5f2b
simocampi <simocampy@gmail.com>
2019-03-23 15:40:02 +0100

    Trigger1 INIT

--- guiIS/src/database/config/TriggersTest.java
+++ guiIS/src/database/config/TriggersTest.java
@@ -1,5 +1,7 @@
 package database.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TriggersTest {
@@ -7,8 +9,14 @@ public class TriggersTest {
     private static final String URL="jdbc:h2:file:C:/Users/simoc/IdeaProjects/LaTazza/guiIS/src/database/config";
     public static void main(String[] args) throws SQLException {
 
-        Trigger1 trigger1= new Trigger1(URL);
+        Connection conn= DriverManager.getConnection(URL);
 
+        Trigger1 trigger1= new Trigger1(conn);
 
+        trigger1.getStat().execute("CREATE TRIGGER CHECKNUMCIALDE " +
+                "AFTER UPDATE ON LATAZZASCHEMA.COMPRA_VISITATORE FOR EACH ROW " +
+                "CALL \"Trigger1.class.getname()\" ");
+
+        trigger1.close();
     }
 }
