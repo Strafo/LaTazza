d0f86762183e9f67fbfda19c611229989a144e02
LazyRacc00n <37672349+LazyRacc00n@users.noreply.github.com>
2019-03-25 18:55:50 +0100

    modifiche a ControllerPersonale

--- guiIS/src/backend/ControllerPersonale.java
+++ guiIS/src/backend/ControllerPersonale.java
@@ -1,22 +1,38 @@
 package backend;
 
 import backend.clientpkg.Personale;
+import backend.daopkg.gateways.DaoManager;
+import backend.daopkg.gateways.PersonaleDao;
 
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ControllerPersonale {
     private ArrayList<Personale> list;
+  //  private DaoManager<> dao;
+    private Connection connectionDB;
 
-    public ControllerPersonale(){
+    public ControllerPersonale(Connection c){
         list= new ArrayList<Personale>();
+       // dao= new Da(c);
+        connectionDB=c;
+        initList();
     }
 
+    private void initList(){
+        /*List<Personale> dipendenti=dao.getAll();
+        for (Personale p: dipendenti) {
+            Personale dip= new Personale();
+        }*/
+    }
 
     public List<Personale> getCopyList(){
         List<Personale> l= new ArrayList<>(list);
         return l;
     }
+
     public void aggiungiPersonale(String nome, String cognome){
 
         Personale p=new Personale(nome,cognome);
@@ -24,7 +40,7 @@ public class ControllerPersonale {
         list.add(p);
     }
 
-    public void rimuoviPersonale(Personale p){
+    public void licenziaPersonale(Personale p){
         list.remove(p);
     }
 }
