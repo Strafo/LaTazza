d98152909650dd81c915151d35baca21f45cc122
simocampi <simocampy@gmail.com>
2019-03-25 09:59:26 +0100

    Trigger1 INIT

--- guiIS/src/database/config/TriggersTest.java
+++ guiIS/src/database/config/TriggersTest.java
@@ -3,20 +3,23 @@ package database.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TriggersTest {
 
     private static final String URL="jdbc:h2:file:C:/Users/simoc/IdeaProjects/LaTazza/guiIS/src/database/config";
     public static void main(String[] args) throws SQLException {
-
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         Connection conn= DriverManager.getConnection(URL);
 
-        Trigger1 trigger1= new Trigger1(conn);
+        Statement stat= conn.createStatement();
+        stat.execute("CREATE TRIGGER check_num_Cialde " +
+                "AFTER INSERT ON LATAZZASCHEMA.COMPRA_VISITATORE FOR EACH ROW " +
+                "CALL \"database.congig.TriggersTest$Trigger1\" ");
 
-        trigger1.getStat().execute("CREATE TRIGGER CHECKNUMCIALDE " +
-                "AFTER UPDATE ON LATAZZASCHEMA.COMPRA_VISITATORE FOR EACH ROW " +
-                "CALL \"Trigger1.class.getname()\" ");
-
-        trigger1.close();
     }
 }
