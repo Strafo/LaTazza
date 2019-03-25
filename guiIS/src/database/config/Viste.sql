33e7c84c7f414a63423167a759027c16c9b9ccd7
simocampi <simocampy@gmail.com>
2019-03-25 09:59:26 +0100

    Trigger1 INIT

--- guiIS/src/database/config/Viste.sql
+++ guiIS/src/database/config/Viste.sql
@@ -146,6 +146,25 @@ from (select importoV
       from ImportoRifornimenti());
 
 
+create  view  Magazzino(tipoCialda, qta) as
+  select TIPO_CIALDA, sum(qta*50)
+  from  LATAZZASCHEMA.RIFORNIMENTO
+  group by TIPO_CIALDA
+
+    except
+
+  select tipo_cialda, sum(numero_cialde)
+  from LATAZZASCHEMA.COMPRA_VISITATORE
+  group by tipo_cialda
+
+    except
+
+  select tipo_cialda, sum(numero_cialde)
+  from LATAZZASCHEMA.COMPRA_DIPENDENTE
+  group by tipo_cialda;
+
+
+
 
 
 
