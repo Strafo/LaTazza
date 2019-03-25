f899cbc21299dd9fb78c1b91eeab31a08ec70d01
simocampi <simocampy@gmail.com>
2019-03-25 09:59:26 +0100

    Trigger1 INIT

--- guiIS/src/database/config/trigger.sql
+++ guiIS/src/database/config/trigger.sql
@@ -1,32 +1,6 @@
 
 set schema LATAZZASCHEMA;
 
-create  view  Magazzino(tipoCialda, qta) as
-  select TIPO_CIALDA, sum(qta*50)
-  from  LATAZZASCHEMA.RIFORNIMENTO
-  group by TIPO_CIALDA
-
-    except
-
-  select tipo_cialda, sum(numero_cialde)
-  from LATAZZASCHEMA.COMPRA_VISITATORE
-  group by tipo_cialda
-
-    except
-
-  select tipo_cialda, sum(numero_cialde)
-  from LATAZZASCHEMA.COMPRA_DIPENDENTE
-  group by tipo_cialda;
-
-
-
-
-CREATE TRIGGER check_numero_aquisto_scatole
-AFTER UPDATE OR INSERT ON LATAZZASCHEMA.COMPRA_VISITATORE
-FOR EACH ROW
-EXECUTE PROCEDURE checkNumCialde();
-
-CREATE TRIGGER check_numero_aquisto_scatole
-AFTER UPDATE OR INSERT ON LATAZZASCHEMA.COMPRA_DIPENDENTE
-FOR EACH ROW
-EXECUTE PROCEDURE checkNumCialde();
+CREATE TRIGGER check_num_Cialde_Comprate
+AFTER UPDATE ON lATAZZASCHEMA.COMPRA_VISITATORE FOR EACH ROW
+CALL "database.config.Trigger1";
