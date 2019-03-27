8d535192cc30858d0ab8da67ffa9fe46378fb14d
simocampi <simocampy@gmail.com>
2019-03-27 16:16:47 +0100

    Trigger Test, caricamento tabelle da file e inserimento funzionante

--- guiIS/src/database/config/TriggersTest.java
+++ guiIS/src/database/config/TriggersTest.java
@@ -1,29 +1,82 @@
 package database.config;
 
+import database.DataBase;
+
+import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 public class TriggersTest {
 
-    private static final String URL="jdbc:h2:file:C:/Users/simoc/IdeaProjects/LaTazza/guiIS/src/database/config";
-    public static void main(String[] args) throws SQLException {
+    private Connection conn;
+    private DataBase database;
+    private static final String URL="jdbc:h2:mem:databaseTest";
+    //private static final String URL="jdbc:h2:C:/Users/simoc/IdeaProjects/LaTazza/guiIS/src/database/config";
+    private final String PATH="guiIS\\src\\database\\config\\";
+    private Scanner inFile;
+    private boolean schemaExists=false;
+
+
+
+
+    public TriggersTest() throws SQLException, ClassNotFoundException {
         try {
             Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        Connection conn= DriverManager.getConnection(URL);
+        database= new DataBase(URL);
+        database.initDataBase();
+        conn = database.getConnection();
+    }
 
 
+    public void updateTable(String sqlFileName) throws SQLException {
 
+        try {
+            StringBuilder file= new StringBuilder();
+            inFile= new Scanner(new FileReader(PATH+sqlFileName));
+            while(inFile.hasNext()) {
+                file.append(inFile.nextLine()).append("\n");
+            }
+            //System.out.println("----------------------------------\n"+schema+"\n------------------------");
+            Statement stmt=conn.createStatement();
+            stmt.addBatch(file.toString());
+            stmt.executeUpdate(file.toString());
+            stmt.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            schemaExists=false;
+            System.exit(0);
+            inFile.close();
+            conn.close();
+        }
+        System.out.println("File red and updated");
+        schemaExists=true;
+
+    }
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+
+        TriggersTest T= new TriggersTest();
+        Connection conn= T.getDatabase().getConnection();
         Statement stat= conn.createStatement();
-        //ResultSet rs=stat.executeQuery();
+        T.updateTable("databaseConfig.sql");
+        T.updateTable("Insert.sql");
 
+
+        /*
         stat.execute("CREATE TRIGGER check_num_Cialde " +
                 "AFTER INSERT ON LATAZZASCHEMA.COMPRA_VISITATORE FOR EACH ROW " +
                 "CALL \"TriggersTest$Trigger1\" ");
+        */
+        T.getDatabase().closeDataBase();
     }
+
+    DataBase getDatabase(){return database;}
 }
