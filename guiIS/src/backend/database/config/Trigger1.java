6729b01d7e02bb64aa969d0ceaf3f64d7f935376
simocampi <simocampy@gmail.com>
2019-03-27 15:02:25 +0100

    Trigger1, Trigger Test(NON ANCORA FUNZIONANTE)

--- guiIS/src/backend/database/config/Trigger1.java
+++ guiIS/src/backend/database/config/Trigger1.java
@@ -1,42 +1,21 @@
 package backend.database.config;
 import org.h2.api.Trigger;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Trigger1 implements Trigger {
 
-    // private Connection conn;
-    //private Statement stat;
-    //private String triggerName, table,schema;
-    //private boolean before;
-    //private int type; //1: Insert, 2: Update, 4: Delete, 8: select
-    //private ResultSet cV,cP,magazzino;
 
     @Override
     public void init(Connection connection, String s, String t1, String t2, boolean b, int i) throws SQLException {
 
-        //conn=connection; schema=s; triggerName =t1; table =t2; before=b; type=i;
-        //stat=conn.createStatement();
-        /*cV=stat.executeQuery("select sum(numero_cialde) "+
-                " from LATAZZASCHEMA.COMPRA_VISITATORE T "+
-                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_VISITATORE where T.TIPO_CIALDA=COMPRA_VISITATORE.TIPO_CIALDA); ");
-        cP=stat.executeQuery("select sum(numero_cialde)\n"+
-                " from LATAZZASCHEMA.COMPRA_DIPENDENTE\n"+
-                " where tipo_cialda = (select tipo_cialda from LATAZZASCHEMA.COMPRA_DIPENDENTE T where T.TIPO_CIALDA=COMPRA_DIPENDENTE.TIPO_CIALDA);");
 
-        magazzino=stat.executeQuery("select sum(QTA*50)\n"+
-                " from LATAZZASCHEMA.MAGAZZINO\n"+
-                " where tipoCialda=(select TIPOCIALDA from LATAZZASCHEMA.MAGAZZINO T where T.TIPOCIALDA=MAGAZZINO.TIPOCIALDA);");
-        */
     }
 
-    /* public Trigger1(Connection conn) throws SQLException {
 
-
-         init(conn,"LATAZZASCHEMA","CHECKNUMCIALDE","LATAZZASCHEMA.COMPRA_VISITATORE",false,1);
-
-     }
- */
     @Override
     public void fire(Connection conn, Object[] oldRow, Object[] newRow) throws SQLException {
         PreparedStatement stat;
--- guiIS/src/database/config/TriggersTest.java
+++ guiIS/src/database/config/TriggersTest.java
@@ -1,29 +1,98 @@
 package database.config;
 
+import database.DataBase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class TriggersTest {
 
-    private static final String URL="jdbc:h2:file:C:/Users/simoc/IdeaProjects/LaTazza/guiIS/src/database/config";
-    public static void main(String[] args) throws SQLException {
+    private Connection conn;
+    private DataBase database;
+    private static final String URL="jdbc:h2:mem:default";
+
+
+    public TriggersTest() throws SQLException, ClassNotFoundException {
+        database= new DataBase(URL);
+        database.initDataBase();
+    }
+
+    public DataBase getDatabase(){ return database;}
+
+    public void updateTable(String sqlFilePath) {
+
+        String currentDirPath = System.getProperty("user.dir");
+
+        //String[] pathPart=currentDirPath.split("LaTazza/guiIs");//tapullata incredibile :D
+       String path=currentDirPath+sqlFilePath;//pathPart[0];
+        System.out.println("PATHHHH:  "+path);
+
+        //PER IL DEBUGGING
+        /*System.out.println("Current dir using System:" +currentDirPath);
+        for(String i:pathPart) {
+            System.out.println("part:" +i);
+        }fail("END");*/
+
+        try {
+            //legge il file sql e lo suddivide in query
+            File file = new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+            String str = new String(data, "UTF-8");
+            String[] parts=str.split(";");
+
+            //PER DEBUGGING
+            /*System.out.println("START:");int j=0;
+            for(String i:parts) {
+                System.out.println("part["+j+"]:" +i);
+                j++;
+            }
+            System.out.println("END");*/
+
+
+            conn = database.getConnection();
+            Statement statement = conn.createStatement();
+            //esegue le query
+            for(String i:parts) {
+                statement.execute(i);
+            }
+
+        } catch (IOException | SQLException e) {
+            fail(e.getMessage());
+        }
+    }
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+    System.out.println("eeeeeeeeeeee "+ System.getProperty("user.dir"));
+
+
         try {
             Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        Connection conn= DriverManager.getConnection(URL);
-
-
-
+        TriggersTest T= new TriggersTest();
+        Connection conn= T.getDatabase().getConnection();
         Statement stat= conn.createStatement();
-        //ResultSet rs=stat.executeQuery();
+        T.updateTable("\\guiIS\\src\\database\\config\\databaseConfig.sql");
+        T.updateTable("\\guiIS\\src\\database\\config\\DefaultEntrySet.sql");
 
+        //ResultSet rs=stat.executeQuery();
+        /*
         stat.execute("CREATE TRIGGER check_num_Cialde " +
                 "AFTER INSERT ON LATAZZASCHEMA.COMPRA_VISITATORE FOR EACH ROW " +
                 "CALL \"TriggersTest$Trigger1\" ");
+        */
     }
 }
