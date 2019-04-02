1ea42f59b2b60e66af0f6bf6eef60e8edf860023
simocampi <simocampy@gmail.com>
2019-04-02 17:23:34 +0200

    Risolto problema in Debito

--- guiIS/src/backend/database/config/TriggerPagamentoDebito.java
+++ guiIS/src/backend/database/config/TriggerPagamentoDebito.java
@@ -5,7 +5,7 @@ import org.h2.api.Trigger;
 import java.sql.*;
 
 
-public class TriggerPagamentoDebito implements Trigger {
+public class TriggerPagamentoDebito extends TriggerDebito implements Trigger {
 
     private static final String TRIGGER_PATH = "\"backend.database.config.TriggerPagamentoDebito\"";
     private static final String TABLE_NAME_DEBITO = "LATAZZASCHEMA.DEBITO";
@@ -15,31 +15,20 @@ public class TriggerPagamentoDebito implements Trigger {
 
     private static double getDebitoPagato(Connection conn, Object[] newRow) throws SQLException {
 
+
         PreparedStatement stat = conn.prepareStatement("select importo " +
                 "from " + TABLE_NAME +
                 " where nome=? and cognome=? and data= ? ");
 
         stat.setNString(1, (String) newRow[0]);
         stat.setNString(2, (String) newRow[1]);
+        System.out.println("NewRo0: "+ newRow[0]+" NewRow1: "+newRow[1]+" NewRow2: "+newRow[3]);
+
         stat.setTimestamp(3, (Timestamp) newRow[2]);
-
         ResultSet rs = stat.executeQuery();
-        rs.next();
-        return rs.getDouble(1);
+        if(rs.next()) {rs.getDouble(1); System.out.println("debito Pagato: "+rs.getDouble(1));}
+            return 0.0;
     }
-
-    private static double getDebitoAggiornato(Connection conn, Object[] newRow) throws SQLException {
-
-        ResultSet rs;
-        PreparedStatement stat= conn.prepareStatement("select importo from "+TABLE_NAME_DEBITO+
-                                                           " where nome=? and cognome=?");
-        stat.setNString(1, (String) newRow[0]);
-        stat.setNString(2, (String) newRow[1]);
-        rs=stat.executeQuery();
-        rs.next();
-        return rs.getDouble(1)-getDebitoPagato(conn, newRow);
-    }
-
 
     @Override
     public void init(Connection connection, String s, String s1, String s2, boolean b, int i) throws SQLException {
@@ -49,25 +38,25 @@ public class TriggerPagamentoDebito implements Trigger {
     @Override
     public void fire(Connection conn, Object[] oldRow, Object[] newRow) throws SQLException {
 
-        double debitoAggiornato=getDebitoAggiornato(conn,newRow);
-        //System.out.println(debitoAggiornato);
+        double debitoAggiornato=getCurrentDebito(conn,newRow)-getDebitoPagato(conn,newRow);
+
+        System.out.println("Debito Aggiornato: "+debitoAggiornato);
         PreparedStatement stat= conn.prepareStatement("update "+TABLE_NAME_DEBITO+" set importo="
                 +debitoAggiornato+" where nome=? and cognome=? ");
-
         stat.setNString(1, (String) newRow[0]);
         stat.setNString(2, (String) newRow[1]);
         stat.executeUpdate();
 
 
-
+        /*
         ResultSet rs;
         PreparedStatement prep;
         prep=conn.prepareStatement("select *" +
                 "from LATAZZASCHEMA.DEBITO " );
         rs=prep.executeQuery();
         while(rs.next())
-            System.out.println("2 : \n"+rs.getString(1) + ", " + rs.getString(2));
-
+            System.out.println("2 : \n"+rs.getString(1) + ", " + rs.getString(2)+", "+rs.getDouble(3));
+        */
 
     }
 
