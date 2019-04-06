f8b5e5a4258ee5c64b64891c08513b4260283746
LazyRacc00n <37672349+LazyRacc00n@users.noreply.github.com>
2019-04-06 10:23:06 +0200

    Test

---
+++ guiIS/test/testBackend/ViewCassaTest.java
@@ -0,0 +1,117 @@
+package testBackend;
+
+import utils.Euro;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ViewCassaTest {
+
+    private static final  String TABLE="LATAZZASCHEMA.CASSA";
+    private TriggersTest t;
+    private Connection c;
+    private static ResultSet rs;
+    private static PreparedStatement stat;
+    private static int cassaEuro=385;
+    private static int cassaCentesimi=49;
+    private static Euro cassaAfterInsert= new Euro(cassaEuro,cassaCentesimi);
+
+    @BeforeEach
+    void setUp(){
+        try {
+            t= new TriggersTest();
+            c=t.getConn();
+            t.initDataBase();
+            executeSelect();
+        }catch (Exception exc){
+            fail(exc.getMessage());
+        }
+    }
+
+
+    private void executeSelect() throws SQLException{
+        stat=c.prepareStatement("SELECT * from " + TABLE);
+        rs=stat.executeQuery();
+    }
+
+
+    @Test
+    void testView() {
+        try {
+
+            assertTrue(rs.next());
+            assertEquals(cassaAfterInsert.getEuro(), rs.getInt(1) );
+            assertEquals(cassaAfterInsert.getCentesimi(), rs.getInt(2));
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+    }
+
+
+    @Test
+    void testRifornimento(){
+        try {
+            int qta=40;
+            Euro importo= new Euro(0,40);
+            importo.moltiplicaImporto(qta);
+            cassaAfterInsert.sottraiImporto(importo);
+            stat=c.prepareStatement("insert into LATAZZASCHEMA.RIFORNIMENTO values ('2019-05-12 12:00:00',"+qta+" ,'caffe')" );
+            rs=stat.executeQuery();
+            assertTrue(rs.next());
+            assertEquals(cassaAfterInsert.getEuro(), rs.getInt(1) );
+            assertEquals(cassaAfterInsert.getCentesimi(), rs.getInt(2));
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+
+    @Test
+    void testPagamentoDebito(){
+        try {
+            Euro importo= new Euro(3,50);
+            stat=c.prepareStatement("insert into LATAZZASCHEMA.PAGAMENTO_DEBITO values ('Jacopo','Dapueto', '2019-03-11 14:00:00',"+cassaAfterInsert.getEuro()+", "+cassaAfterInsert.getCentesimi()+")" );
+            rs=stat.executeQuery();
+            cassaAfterInsert.aggiungiImporto(importo);
+            assertTrue(rs.next());
+            assertEquals(cassaAfterInsert.getEuro(), rs.getInt(1) );
+            assertEquals(cassaAfterInsert.getCentesimi(), rs.getInt(2));
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    void testVendita(){
+        try {
+            int qta=2;
+            Euro importo= new Euro(0,50);
+            importo.moltiplicaImporto(qta);
+            stat=c.prepareStatement("insert into LATAZZASCHEMA.COMPRA_DIPENDENTE values ('Simone','Campisi', 'caffe',"+qta+",'2018-07-11 13:00:00',true)");
+            rs=stat.executeQuery();
+            cassaAfterInsert.aggiungiImporto(importo);
+            assertTrue(rs.next());
+            assertEquals(cassaAfterInsert.getEuro(), rs.getInt(1) );
+            assertEquals(cassaAfterInsert.getCentesimi(), rs.getInt(2));
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+
+
+
+
+
+
+}
--- guiIS/test/testBackend/ViewMagazzinoTest.java
+++ guiIS/test/testBackend/ViewMagazzinoTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ViewMagazzinoTest {
 
+    private static final  String TABLE="LATAZZASCHEMA.MAGAZZINO";
     private TriggersTest t;
     private Connection c;
     private static ResultSet rs;
@@ -24,30 +25,54 @@ public class ViewMagazzinoTest {
         try {
             t= new TriggersTest();
             c=t.getConn();
+            t.initDataBase();
         }catch (Exception exc){
             fail(exc.getMessage());
         }
     }
 
 
+    private void executeSelect() throws SQLException{
+        stat=c.prepareStatement("SELECT * from " + TABLE);
+        rs=stat.executeQuery();
+    }
 
 
     @Test
     void testView() {
         try {
-            t.initDataBase();
-            stat=c.prepareStatement("SELECT * from LATAZZASCHEMA.MAGAZZINO");
-            rs=stat.executeQuery();
-            while(rs.next()){
+
+            executeSelect();
+            while (rs.next()) {
                 switch (rs.getString(1)) {
-                    case "caffe": assertEquals(162,rs.getInt(2));
+                    case "caffe":
+                        assertEquals(162, rs.getInt(2));
+                        break;
+                    case "the":
+                        assertEquals(130, rs.getInt(2));
                 }
             }
         } catch (SQLException e) {
             fail(e.getMessage());
         }
-
     }
+
+
+        @Test
+        void testUpdate(){
+            try {
+                executeSelect();
+                stat=c.prepareStatement("update " +TABLE+" set tipo='Camomilla' where tipo='the' " );
+                rs=stat.executeQuery();
+                String the= new String("the");
+                while(rs.next()){
+                    assertFalse(the.equals(rs.getString(1)));
+                }
+            } catch (SQLException e) {
+                fail(e.getMessage());
+            }
+
+        }
 
 
 
