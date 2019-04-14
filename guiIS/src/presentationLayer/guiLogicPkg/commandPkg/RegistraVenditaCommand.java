0163e09bddd18e534511bee795c6623742b0d5a6
strafoxultrapowa <aspem2015@gmail.com>
2019-04-14 15:17:35 +0200

    implementazione RegistraVenditaCommand

--- guiIS/src/presentationLayer/guiLogicPkg/commandPkg/RegistraVenditaCommand.java
+++ guiIS/src/presentationLayer/guiLogicPkg/commandPkg/RegistraVenditaCommand.java
@@ -1,5 +1,11 @@
 package presentationLayer.guiLogicPkg.commandPkg;
 
+import backend.dataAccessLayer.rowdatapkg.CialdeEntry;
+import backend.dataAccessLayer.rowdatapkg.clientPkg.Cliente;
+import backend.dataAccessLayer.rowdatapkg.clientPkg.Personale;
+import backend.dataAccessLayer.rowdatapkg.clientPkg.Visitatore;
+import presentationLayer.guiLogicPkg.BackEndInvoker;
+
 public class RegistraVenditaCommand implements Command {
 
     private String tipoCialda;
@@ -8,17 +14,29 @@ public class RegistraVenditaCommand implements Command {
     private String nome;
     private String cognome;
     private boolean personale;//true se personale,false se visitatore.
+    private BackEndInvoker backEndInvoker;
 
-    public RegistraVenditaCommand(String tipoCialda,boolean contanti,String nome,String cognome,int qta,boolean personale){
+    public RegistraVenditaCommand(String tipoCialda,boolean contanti,String nome,String cognome,int qta,boolean personale,BackEndInvoker backEndInvoker){
         this.contanti=contanti;
         this.nome=nome;
         this.cognome=cognome;
         this.qta=qta;
+        this.tipoCialda=tipoCialda;
+        this.personale=personale;
+        this.backEndInvoker=backEndInvoker;
     }
 
     @Override
     public boolean execute() {
-        return false;
+        Cliente cliente= personale?new Personale(nome,cognome):new Visitatore(nome,cognome);
+        CialdeEntry cialda=new CialdeEntry(tipoCialda);
+        backEndInvoker.getControllerContabilita().registraVendita(
+                cliente,
+                cialda,
+                qta,
+                contanti
+        );
+        return true;
     }
 }
 
