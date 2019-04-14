90869ecfdfa9f804bf8a81c046cd34b29f182549
simocampi <simocampy@gmail.com>
2019-04-14 15:02:18 +0200

    creazione test magazzino

--- guiIS/test/testBackend/MagazzinoTest.java
+++ guiIS/test/testBackend/MagazzinoTest.java
@@ -1,6 +1,7 @@
 package testBackend;
 
 import backend.businessLogicLayer.Magazzino;
+import backend.dataAccessLayer.rowdatapkg.CialdeEntry;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import static junit.framework.Assert.fail;
+import static junit.framework.Assert.*;
 
 class MagazzinoTest {
     private Magazzino magazzino;
@@ -19,7 +20,8 @@ class MagazzinoTest {
     private Connection conn;
     private ResultSet rs;
     private PreparedStatement stat;
-
+    private CialdeEntry caffe;
+    private int qtaScatole;
 
     @BeforeEach
     void setUp() {
@@ -29,7 +31,9 @@ class MagazzinoTest {
             t=new TriggersTest();
             conn=t.getConn();
             t.initDataBase();
-            //magazzino=new Magazzino();
+            magazzino=null;
+            qtaScatole=magazzino.getQtaCialdeScatole();
+            caffe= new CialdeEntry("caffe");
 
         }catch (Exception e){
             e.printStackTrace();
@@ -42,11 +46,20 @@ class MagazzinoTest {
     @Test
     void aggiungiScatole() {
 
-
+        Integer oldQta= magazzino.getCopyStato().get(caffe);
+        assertTrue(magazzino.aggiungiScatole(caffe,3));
+        int newQta=oldQta+(qtaScatole*3);
+        int getNewQta= magazzino.getCopyStato().get(caffe);
+        assertEquals(getNewQta, newQta );
     }
 
     @Test
     void rimuoviCialde() {
+        Integer oldQta= magazzino.getCopyStato().get(caffe);
+        assertTrue(magazzino.rimuoviCialde (caffe,1));
+        int newQta=oldQta-qtaScatole;
+        int getNewQta= magazzino.getCopyStato().get(caffe);
+        assertEquals(getNewQta, newQta );
     }
 
 
