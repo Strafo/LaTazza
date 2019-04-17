6a4b59e64c47f780b329086c8b2e88bf7588b220
simone <simocampy@gmail.com>
2019-04-17 17:58:02 +0200

    Classe PagamentoDebitoCommand funzionante, Cambiato anche in RegPagamentoPropreties, la formattazione  del textfield per l'importo del pagamento debito, serviva formattazione Decimal

--- LaTazzaModule/src/businessLogicLayer/commandPkg/PagamentoDebitoCommand.java
+++ LaTazzaModule/src/businessLogicLayer/commandPkg/PagamentoDebitoCommand.java
@@ -1,8 +1,25 @@
 package businessLogicLayer.commandPkg;
 
+import businessLogicLayer.BackEndInvoker;
+import utils.Euro;
+
 public class PagamentoDebitoCommand implements Command {
+
+    private String nome,cognome;
+    private Euro importo;
+    private BackEndInvoker backEndInvoker;
+
+    public PagamentoDebitoCommand(String nome, String cognome, Euro importo, BackEndInvoker backEndInvoker){
+        this.nome=nome;
+        this.cognome= cognome;
+        this.importo=importo;
+        this.backEndInvoker=backEndInvoker;
+
+    }
+
     @Override
     public boolean execute() throws Exception {
-        return false;
+      boolean result= backEndInvoker.getControllerDebito().registrarePagamentoDebito(importo,nome,cognome);
+      return result;
     }
 }
--- LaTazzaModule/src/presentationLayer/guiLogicPkg/contentsPanelsPkg/RegistraPagamento.java
+++ LaTazzaModule/src/presentationLayer/guiLogicPkg/contentsPanelsPkg/RegistraPagamento.java
@@ -3,9 +3,12 @@ package presentationLayer.guiLogicPkg.contentsPanelsPkg;
 import javax.swing.*;
 
 import businessLogicLayer.ControllerPersonale;
+import businessLogicLayer.commandPkg.PagamentoDebitoCommand;
 import dataAccessLayer.rowdatapkg.clientPkg.Personale;
 import presentationLayer.guiConfig.contentsPanelsPropertiesPkg.RegPagamentoProperties;
 import presentationLayer.LaTazzaApplication;
+import utils.Euro;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -36,8 +39,6 @@ public class RegistraPagamento extends AbstractPanel {
 		add(textFieldAmmontare=RegPagamentoProperties.createAndInitJFormattedTextFieldAmmontare());
 		add(buttonConferma=RegPagamentoProperties.createAndInitJButtonConferma());
 		add(buttonAnnulla=RegPagamentoProperties.createAndInitJButtonAnnulla());
-
-
 		buttonAnnulla.addMouseListener(
 				new MouseAdapter() {
 					@Override
@@ -46,8 +47,20 @@ public class RegistraPagamento extends AbstractPanel {
 					}
 				}
 		);
+
+		buttonConferma.addMouseListener(
+				new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						conferma();
+					}
+				}
+		);
+
 		LaTazzaApplication.backEndInvoker.addObserver(PERSONALELIST,this);
+
 	}
+
 
     private void setComboBoxNomePersonale(List<Personale> lista){
 		nomePersonaleMenu.removeAllItems();
@@ -65,6 +78,21 @@ public class RegistraPagamento extends AbstractPanel {
         nomePersonaleMenu.setSelectedIndex(0);
         textFieldAmmontare.setValue(null);
     }
+
+    private void conferma(){
+		String personale= (String) nomePersonaleMenu.getSelectedItem();
+		String[] nomeCognome= personale.split(" ");
+		String[] euroCent=  textFieldAmmontare.getText().split("\\.");
+		long euro= Long.valueOf(euroCent[0]);
+		int  cent= Integer.valueOf(euroCent[1]);
+		Euro importo=new Euro(euro, cent);
+		PagamentoDebitoCommand command= new PagamentoDebitoCommand(nomeCognome[0],nomeCognome[1],importo,LaTazzaApplication.backEndInvoker);
+		if(!LaTazzaApplication.backEndInvoker.executeCommand(command))
+			System.err.println("Errore nel pagamento del Debito");
+		else
+			System.err.println("Debito Pagato Correttamente");
+
+	}
 
     @Override
     public void update(Observable o, Object arg) {
